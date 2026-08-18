452b9274b0b0e047c2d8ec88e639681599629733
NoahGNielsen <github.com.1ersk@passinbox.com>
2026-08-18 22:42:54 +0200

    added config file

---
+++ Good Reads Prog Project/config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Good_Reads_Prog_Project
+{
+    internal class config
+    {
+        string DBAccessString = @"Data Source=XXXX;Initial Catalog=XXXX;Integrated Security=True;TrustServerCertificate=True";
+    }
+}
