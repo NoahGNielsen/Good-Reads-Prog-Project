2c18b1171680c1f87e4fdbc7676ed34d6e4de937
NoahGNielsen <github.com.1ersk@passinbox.com>
2026-08-21 08:52:34 +0200

    Added some forms to the project

---
+++ Good Reads Prog Project/Anbefalinger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Good_Reads_Prog_Project
+{
+    public partial class Anbefalinger : Form
+    {
+        public Anbefalinger()
+        {
+            InitializeComponent();
+        }
+    }
+}
