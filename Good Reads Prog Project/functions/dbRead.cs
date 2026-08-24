6677ecc8fcf6846e7b85a53eed6a50abbbc66b02
NoahGNielsen <github.com.1ersk@passinbox.com>
2026-08-24 12:46:52 +0200

    added new file, db read

---
+++ Good Reads Prog Project/functions/dbRead.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+
+namespace Good_Reads_Prog_Project.functions
+{
+    public class dbRead
+    {
+        public static string ReadData(string query)
+        {
+            var config = new Config();
+            string result = string.Empty;
+            using (var connection = new Microsoft.Data.SqlClient.SqlConnection(config.DBAccessString))
+            {
+                try
+                {
+                    connection.Open();
+                    using (var command = new Microsoft.Data.SqlClient.SqlCommand(query, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Assuming you want to read the first column as a string
+                                result += reader[0].ToString() + Environment.NewLine;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions (e.g., log them)
+                    result = "Error: " + ex.Message;
+                }
+            }
+            return result;
+        }
+    }
+}
